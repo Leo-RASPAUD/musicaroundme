42396ab3d8fa17e1e1374dd4aad6c35fb5b0d7e7
Léo RASPAUD <leo.raspaud@gmail.com>
2018-08-25 13:35:33 +1000

    Remove theme default

--- src/index.js
+++ src/index.js
@@ -8,8 +8,6 @@ import thunkMiddleware from 'redux-thunk';
 import { ConnectedRouter, routerMiddleware } from 'react-router-redux';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import mainReducer from 'reducers/main.reducer';
-import indigo from '@material-ui/core/colors/indigo';
-import pink from '@material-ui/core/colors/pink';
 import App from 'components/App/App.container';
 import './assets/app.css';
 import 'animate.css';
@@ -31,12 +29,7 @@ if (window.__REDUX_DEVTOOLS_EXTENSION__) {
 }
 
 const store = createStore(mainReducer, compose(...composeFunctions));
-const theme = createMuiTheme({
-    palette: {
-        primary: indigo,
-        secondary: pink,
-    },
-});
+const theme = createMuiTheme({});
 
 ReactDOM.render(
     <Provider store={store}>
