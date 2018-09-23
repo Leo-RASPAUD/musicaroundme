50b034011e67c126de3d3c50d891e95601e7d7d6
Léo RASPAUD <leo.raspaud@gmail.com>
2018-09-23 12:32:28 +1000

    Fix duplicates markers

--- src/components/VenueMarkers/VenueMarkers.component.js
+++ src/components/VenueMarkers/VenueMarkers.component.js
@@ -1,33 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import uuid from 'uuid';
 import { Marker, OverlayView } from 'react-google-maps';
 import VenueInfo from 'components/VenueInfo/VenueInfo.component';
 
 const VenueMarkers = props => {
     const { venues, zoomOnVenue } = props;
-    return venues.map(venue => (
-        <Marker
-            key={venue.id}
-            position={venue}
-            animation={google.maps.Animation.DROP}
-            onClick={() =>
-                zoomOnVenue({
-                    position: { lat: venue.lat, lng: venue.lng },
-                    zoom: 16,
-                    venueId: venue.id,
-                })
-            }
-        >
-            {venue.selected && (
-                <OverlayView
-                    mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
-                    position={{ lat: venue.lat, lng: venue.lng }}
-                >
-                    <VenueInfo venue={venue} />
-                </OverlayView>
-            )}
-        </Marker>
-    ));
+    return venues.map(venue => {
+        const position = { lat: venue.lat, lng: venue.lng };
+        return (
+            <Marker
+                key={venue.id || uuid()}
+                position={position}
+                animation={google.maps.Animation.DROP}
+                onClick={() =>
+                    zoomOnVenue({
+                        position,
+                        zoom: 16,
+                        venueId: venue.id,
+                    })
+                }
+            >
+                {venue.selected && (
+                    <OverlayView mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET} position={position}>
+                        <VenueInfo venue={venue} />
+                    </OverlayView>
+                )}
+            </Marker>
+        );
+    });
 };
 
 VenueMarkers.propTypes = {
--- src/reducers/venues.reducer.js
+++ src/reducers/venues.reducer.js
@@ -9,11 +9,33 @@ const initialState = {
 
 const appReducer = (state = initialState, action) => {
     switch (action.type) {
-        case states.GET_UPCOMING_EVENTS.success:
+        case states.GET_UPCOMING_EVENTS.success: {
+            const currentPosition = {};
+            const venuesNoDuplicates = [];
+            const venues = action.upcomingEvents.map(event => event.venue);
+
+            for (let i = 0; i < venues.length; i += 1) {
+                const element = venues[i];
+                if (!element.id) {
+                    element.id = -1;
+                    currentPosition.lat = element.lat;
+                    currentPosition.lng = element.lng;
+                }
+                if (!element.lat || !element.lng) {
+                    element.lat = currentPosition.lat;
+                    element.lng = currentPosition.lng;
+                }
+
+                const alreadyExists = venuesNoDuplicates.find(venue => venue.id === element.id);
+                if (!alreadyExists) {
+                    venuesNoDuplicates.push(element);
+                }
+            }
             return {
                 ...state,
-                items: action.upcomingEvents.map(item => item.venue),
+                items: venuesNoDuplicates,
             };
+        }
         case states.ZOOM_ON_VENUE:
             return {
                 ...state,
